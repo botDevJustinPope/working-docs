17bd8acf2e5aae8a8e5287c41367215c164395d6
Justin Pope <jusitnpo@buildontechnologies.com>
2025-05-22 15:03:43 -0500

    update angular tutorial

--- ZeroToMastery/Angular/Projects/angular-basics/src/app/app.component.ts
+++ ZeroToMastery/Angular/Projects/angular-basics/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from "@angular/core";
+import { PostComponent } from "./post/post.component";
 
 @Component({
   selector:'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  imports: [PostComponent],
 })
 export class AppComponent {
   name = signal('Justin');
@@ -11,5 +14,9 @@ export class AppComponent {
 
   getName() {
     return this.name();
+  }
+
+  changeImage(e: KeyboardEvent) {
+    this.imageURL.set((e.target as HTMLInputElement).value);
   }
 }
---
+++ ZeroToMastery/Angular/Projects/angular-basics/src/app/post/post.component.ts
@@ -0,0 +1,12 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  standalone: true,
+  imports: [],
+  templateUrl: './post.component.html',
+  styleUrl: './post.component.scss'
+})
+export class PostComponent {
+  postImage = input('');
+}
