f891a9c4402424acb0fd3faee3ed5a9c506bc85a
Justin Pope <jusitnpo@buildontechnologies.com>
2025-06-04 09:12:07 -0500

    update angular project

--- ZeroToMastery/Angular/Projects/VDS-Angular/src/app/app.component.ts
+++ ZeroToMastery/Angular/Projects/VDS-Angular/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PackagesComponent } from './components/packages/packages.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [PackagesComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- ZeroToMastery/Angular/Projects/VDS-Angular/src/app/components/packages/packages.component.ts
+++ ZeroToMastery/Angular/Projects/VDS-Angular/src/app/components/packages/packages.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-packages',
+  standalone: true,
   imports: [],
   templateUrl: './packages.component.html',
   styleUrl: './packages.component.scss'
