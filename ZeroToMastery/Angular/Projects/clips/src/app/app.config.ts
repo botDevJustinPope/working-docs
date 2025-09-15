f19fd31dd876d239fc22659833c5b254e95195f1
Justin Pope <jusitnpo@buildontechnologies.com>
2025-09-15 07:07:32 -0500

    changes

--- ZeroToMastery/Angular/Projects/clips/src/app/app.config.ts
+++ ZeroToMastery/Angular/Projects/clips/src/app/app.config.ts
@@ -1,8 +1,27 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp(
+        // clips's Firebase configuration
+        {
+          apiKey: 'AIzaSyDfpeTLLxQ3S44-d5TIho31fDmkcoJIU1A',
+          authDomain: 'clips-bfb31.firebaseapp.com',
+          projectId: 'clips-bfb31',
+          storageBucket: 'clips-bfb31.firebasestorage.app',
+          messagingSenderId: '48076583695',
+          appId: '1:48076583695:web:f0f0d43398369f5674447a',
+        }
+      )
+    ),
+    provideAuth(() => getAuth()),
+  ],
 };
